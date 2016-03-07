a7f84a27bb4958c9ea345f9b1d490959ed91ff62
Christos Matskas <christos.matskas@gmail.com>
2016-03-07 22:55:37 +0000

    Sample code updated
    For the blog post

--- scripts/site.js
+++ scripts/site.js
@@ -1,24 +1,34 @@
-//PDFJS.disableWorker = ;
-PDFJS.getDocument('alphatrans.pdf').then(function(pdf){
+var progressHandler = function (progress){
+    console.info('Loaded ' + progress.loaded + ' of ' + progress.total);
+}
+
+PDFJS.getDocument('pdf.pdf').then(function(pdf){
+    
+    console.info('Document contains ' + pdf.pdfInfo.numPages + ' pages');
+
+    //assign the progress event handler
+    pdf.loadingTask.onProgress = progressHandler;
+        
     // Fetch the page.
     pdf.getPage(1).then(function (page) {
 
-      // Prepare canvas using PDF page dimensions.
-      var containerColumn = document.getElementById('pdfContainer');
-      var canvas = document.getElementById('canvas');
-      var context = canvas.getContext('2d');
-      canvas.height = containerColumn.clientHeight;
-      canvas.width = containerColumn.clientWidth;
+        // Prepare canvas using PDF page dimensions.
+        var containerColumn = document.getElementById('pdfContainer');
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        canvas.height = containerColumn.clientHeight;
+        canvas.width = containerColumn.clientWidth;
 
-      var unscaledViewport = page.getViewport(1);
-      var scale = Math.min((canvas.height / unscaledViewport.height), (canvas.width / unscaledViewport.width));
-      var viewport = page.getViewport(scale);
-      
-      // Render PDF page into canvas context.
-      var renderContext = {
+        var unscaledViewport = page.getViewport(1);
+        var scale = Math.min((canvas.height / unscaledViewport.height), (canvas.width / unscaledViewport.width));
+        var viewport = page.getViewport(scale);
+        
+        // Render PDF page into canvas context.
+        var renderContext = {
         canvasContext: context,
         viewport: viewport
-      };
-      page.render(renderContext);
+        };
+        page.render(renderContext);
     });
-  });
+});
+  
